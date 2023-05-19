155c883653dd9b823964027c39a091a09e67f8d3
dsaint31 <dsaint31@gmail.com>
2023-05-19 18:55:53 +0900

    add png

--- ds_main.py
+++ ds_main.py
@@ -2,6 +2,9 @@ import cv2
 import sys, os
 import ds_imread
 
+
+# -------------------------------------------------
+# track bar설정.
 alpha_slider_max = 300
 beta_slider_max = 100
 
@@ -41,8 +44,10 @@ def map(x, in_min, in_max, out_min, out_max):
     return int((x-in_min) * (out_max-out_min) / (in_max-in_min) + out_min)
     
 def apply_brightness_contrast(input_img, brightness = 255, contrast = 127):
+    
     brightness = map(brightness, 0, 510, -255, 255) # -255~255범위로 변환 ratio
     contrast   = map(contrast,   0, 254, -127, 127) # -127~127범위로 변환 ratio
+    print(brightness,contrast)
     
     if brightness != 0:
         if brightness > 0:
@@ -63,7 +68,8 @@ def apply_brightness_contrast(input_img, brightness = 255, contrast = 127):
         gamma_c = 127*(1-f)
         buf = cv2.addWeighted(buf, alpha_c, buf, 0, gamma_c)
     cv2.putText(buf,'B:{},C:{}'.format(brightness,contrast),(10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
-    return buf    
+    return buf
+# end track bar 설정 ------------------------------------------------------
     
 def ds_paint(img_ori):
     fstr=''
